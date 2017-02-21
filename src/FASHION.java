8d5907da6691178e37993abac44f5843fa491506
Shahbaz Ahmed <shahbazahmed1269@gmail.com>
2017-02-21 20:43:47 +0530

    Added solution for FASHION problem

---
+++ src/FASHION.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Solution to SPOJ problem FASHION
+ * @see <a href="http://www.spoj.com/problems/FASHION/">http://www.spoj.com/problems/FASHION/</a>
+ * @author Shahbaz Ahmed
+ *
+ */
+public class FASHION {
+
+	public static void main(String[] args) {
+		int count, n, a[], b[], sum;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		while (n-- > 0) {
+			count = sc.nextInt();
+			a = new int[count];
+			b = new int[count];
+			sum = 0;
+			for(int i = 0; i< count; i++) 
+				a[i] = sc.nextInt();
+			for(int i = 0; i< count; i++) 
+				b[i] = sc.nextInt();
+			Arrays.sort(a);
+			Arrays.sort(b);
+			for(int i = 0; i< count; i++) 
+				sum += (a[i]*b[i]);
+			System.out.println(sum);
+		}
+		sc.close();
+	}
+
+}
