c7645bec6ec858867bd948a09591471b0ed87ae9
Shahbaz Ahmed <shahbazahmed1269@gmail.com>
2017-02-26 01:54:06 +0530

    Added 2 more solutions

---
+++ src/AE00.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AE00 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int count = n;
+		for (int i = 2; i <= n/2; i++)
+			for (int j = i; j <= n/2; j++) {
+				if (i * j <= n) 
+					count +=1;
+			}
+		System.out.println(count);
+		sc.close();
+	}
+}
---
+++ src/LASTDIG.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LASTDIG {
+
+	public static void main(String[] args) {
+		int a;
+		long b;
+		Scanner sc = new Scanner(System.in);
+		int n  = sc.nextInt();
+		while (n-- > 0) {
+			a = sc.nextInt();
+			b = sc.nextLong();
+			if (b == 0) {
+				System.out.println(1);
+				continue;
+			}
+			int power = mod(b, 4);
+			if (power != 0)
+				System.out.println((int)(Math.pow(a, power) % 10));
+			else 
+				System.out.println((int)(Math.pow(a, 4) % 10));
+		}
+		sc.close();
+	}
+	
+	public static int mod(long n1, int n2) {
+		return (int) n1 % n2;
+	}
+}
