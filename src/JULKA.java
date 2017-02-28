7a49ad662e7c84742594d5708f9845b48e90c9d9
Shahbaz Ahmed <shahbazahmed1269@gmail.com>
2017-02-28 23:43:34 +0530

    Solved problem JULKA

---
+++ src/JULKA.java
@@ -0,0 +1,27 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * Solution to SPOJ problem JULKA
+ * @see <a href="http://www.spoj.com/problems/JULKA/">http://www.spoj.com/problems/JULKA/</a>
+ * @author Shahbaz Ahmed
+ *
+ */
+public class JULKA {
+
+	public static void main(String[] args) {
+		BigInteger n, res, diff;
+		int cases = 10;
+		 Scanner sc = new Scanner(System.in);
+	     while (cases-- > 0) {
+	    	 n = new BigInteger(sc.next());
+	    	 diff = new BigInteger(sc.next());
+	    	 res = (n.add(diff)).divide(new BigInteger("2"));
+	    	 System.out.println(res.toString());
+	    	 System.out.println((res.subtract(diff)).toString());
+		}
+	    sc.close();
+
+	}
+
+}
