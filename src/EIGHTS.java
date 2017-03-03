7a309f094d577a47d48b4b9676f0a5ea08c6aff0
Shahbaz Ahmed <shahbazahmed1269@gmail.com>
2017-03-03 23:36:15 +0530

    Adding solution for EIGHTS

---
+++ src/EIGHTS.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Solution to SPOJ problem EIGHTS
+ * @see <a href="http://www.spoj.com/problems/EIGHTS/">http://www.spoj.com/problems/EIGHTS/</a>
+ * @author Shahbaz Ahmed
+ *
+ */
+public class EIGHTS {
+
+	public static void main(String[] args) {
+		long n, cal;
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- > 0) {
+			n = sc.nextLong();
+			cal = (n - 1) * 250 + 192;
+			System.out.println(cal);
+		}
+		sc.close();
+	}
+
+}
